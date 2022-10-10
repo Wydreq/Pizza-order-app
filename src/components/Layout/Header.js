76c46559a397836611035a5fd43751193abdbbc5
Bartłomiej Wydrzycki <92253080+Wydreq@users.noreply.github.com>
2022-10-10 16:09:05 +0200

    Updating Css

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,16 +2,15 @@ import React from "react";
 import pizzaImage from "../../assets/pizza.jpg";
 import classes from "./Header.module.css";
 import HeaderCartButton from "./HeaderCartButton";
-import PizzaIcon from "./PizzaIcon";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPizzaSlice } from "@fortawesome/free-solid-svg-icons";
 
 const Header = (props) => {
   return (
     <React.Fragment>
       <header className={classes.header}>
-        <h1>
-          <PizzaIcon />
-          Pizzaliana
-        </h1>
+        <FontAwesomeIcon icon={faPizzaSlice} className={classes.icon} />
+        <h1>Pizzaliana</h1>
         <HeaderCartButton onClick={props.onClick} />
       </header>
       <div className={classes["main-image"]}>
