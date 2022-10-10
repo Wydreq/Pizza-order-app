cce8e50e2faba32a93653b22f6ef3eef3fa3dc5d
Bartłomiej Wydrzycki <92253080+Wydreq@users.noreply.github.com>
2022-10-10 15:49:23 +0200

    Installing bootstrap and font awesome

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,12 +2,16 @@ import React from "react";
 import pizzaImage from "../../assets/pizza.jpg";
 import classes from "./Header.module.css";
 import HeaderCartButton from "./HeaderCartButton";
+import PizzaIcon from "./PizzaIcon";
 
 const Header = (props) => {
   return (
     <React.Fragment>
       <header className={classes.header}>
-        <h1>Pizzaliana</h1>
+        <h1>
+          <PizzaIcon />
+          Pizzaliana
+        </h1>
         <HeaderCartButton onClick={props.onClick} />
       </header>
       <div className={classes["main-image"]}>
